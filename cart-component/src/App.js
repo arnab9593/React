6ea61a428a30b3116c2acb0f3e4d1794ea5a11f8
arnab9593 <arnabpal679@gmail.com>
2022-09-08 11:46:21 +0530

    release 1

---
+++ cart-component/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import './Components/components.css';
+import React from "react";
+import Cart from './Components/Cart';
+
+function App() {
+  return (
+    <div className="App">
+      <Cart />
+    </div >
+  );
+}
+
+export default App;
