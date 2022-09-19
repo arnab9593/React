6818e020cc5e84fae8d0af29003f887e12ec7146
arnab9593 <arnabpal679@gmail.com>
2022-09-19 12:30:42 +0530

    single page sorted

--- cart-psc/src/Pages/SingleProduct.jsx
+++ cart-psc/src/Pages/SingleProduct.jsx
@@ -1,12 +1,45 @@
-
+import { useState, useEffect } from "react";
+import { Link, useParams } from 'react-router-dom';
 import React from "react";
 
+// http://localhost:3030/api/products?id=${products_id}
+
 const SingleProduct = () => {
+
+    const getData = (url) => {
+        return fetch(url).then((res) => res.json())
+    }
+
+    const { products_id } = useParams();
+
+    const [productDetails, setProductDetails] = useState([]);
+
+    useEffect(() => {
+        getData(`http://localhost:3030/api/products?id=${products_id}`).then((res) => {
+            setProductDetails(res)
+            // console.log(res.id);
+        }
+        ).catch((err) => {
+            console.log(err);
+        })
+
+    }, [products_id])
+    console.log(productDetails[0]);
     return (
-        <h2>Single Product</h2>
+        <div key={productDetails.id} >
+            {/* <h2>Single Product</h2>
+            <img src={productDetails.image} alt='error'></img>
+            <p>{productDetails.title}</p> */}
+            {productDetails && productDetails.map((item) =>
+                <div>
+                    <img src={item.image} alt="error"></img>
+                    <p></p>
+                </div>
+
+            )}
+        </div>
 
     )
 }
 
 export default SingleProduct
-
