68d432d0d61d27657b3773e23d508050f6d1da4e
arnab9593 <arnabpal679@gmail.com>
2023-02-04 22:26:36 +0530

    release 3

--- cake-shop/src/Components/Signin.jsx
+++ cake-shop/src/Components/Signin.jsx
@@ -1,9 +1,25 @@
+import { Button, Heading, Input, Link, Text, VStack } from '@chakra-ui/react'
 import React from 'react'
 
 function Signin() {
-  return (
-    <div>Signin</div>
-  )
+    return (
+        <>
+            <VStack border={"1px solid black"} borderRadius={"10px"} h={"40vh"} w={"70vh"} m={"auto auto"} mt={10} padding={10}>
+                <Heading fontSize={25} fontWeight={100} mb={10}>SignIN</Heading>
+                <Input
+                    placeholder='Enter Email'
+                ></Input>
+
+                <Input
+                    placeholder='Enter Password'
+                ></Input>
+                <Button backgroundColor={"#CDDEF6"} w={"100%"}>Signin</Button>
+                <Text>
+                    <Link href='/signup'>I don't have account !</Link>
+                </Text>
+            </VStack>
+        </>
+    )
 }
 
 export default Signin
--- cake-shop/src/Components/Signup.jsx
+++ cake-shop/src/Components/Signup.jsx
@@ -1,9 +1,33 @@
+import { Button, Heading, Input, Link, Text, VStack } from '@chakra-ui/react'
 import React from 'react'
 
 function Signup() {
-  return (
-    <div>Signup</div>
-  )
+    return (
+        <>
+            <VStack border={"1px solid black"} borderRadius={"10px"} h={"60vh"} w={"70vh"} m={"auto auto"} mt={10} padding={10}>
+                <Heading fontSize={25} fontWeight={100} mb={10}>SignUP</Heading>
+                <Input
+                    placeholder='Enter Name'
+                ></Input>
+                <Input
+                    placeholder='Enter Email'
+                ></Input>
+                <Input
+                    placeholder='Enter Phone Number'
+                ></Input>
+                <Input
+                    placeholder='Enter Password'
+                ></Input>
+                <Input
+                    placeholder='Confirm Password'
+                ></Input>
+                <Button backgroundColor={"#CDDEF6"} w={"100%"}>Signup</Button>
+                <Text>
+                    <Link href='/signin'>I already have account !</Link>
+                </Text>
+            </VStack>
+        </>
+    )
 }
 
 export default Signup
