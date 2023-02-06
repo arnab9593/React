ff347a3fc993f484017a4dcc36e4ce95a173e518
arnab9593 <arnabpal679@gmail.com>
2023-02-06 20:13:27 +0530

    release 5

--- cake-shop/src/AllRoutes/AllRoutes.jsx
+++ cake-shop/src/AllRoutes/AllRoutes.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from '../Components/Home'
-import Login from '../Components/Login'
 import Signin from '../Components/Signin'
 import Signup from '../Components/Signup'
 
@@ -10,7 +9,6 @@ function AllRoutes() {
         <>
             <Routes>
                 <Route path='/' element={<Home></Home>}></Route>
-                <Route path='login' element={<Login></Login>}></Route>
                 <Route path='signin' element={<Signin></Signin>}></Route>
                 <Route path='signup' element={<Signup></Signup>}></Route>
             </Routes>
--- cake-shop/src/Components/Home.jsx
+++ cake-shop/src/Components/Home.jsx
@@ -1,10 +1,41 @@
+import React, { useEffect, useState } from 'react'
 
-import React from 'react'
+const getData = () => {
+    return fetch(`https://itchy-coveralls-ox.cyclic.app/api/cakes`).then(res => {
+        return res.json();
+    })
+}
 
 function Home() {
+    const [postData, setData] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const handleApi = async () => {
+        try {
+            setLoading(true)
+            const data = await getData();
+            console.log(data);
+            setData(data[0]);
+            setLoading(false);
+        } catch (e) {
+            setLoading(false);
+            console.log(e.message);
+        }
+    }
+
+    useEffect(() => {
+        handleApi()
+    }, []);
+    if (loading) {
+        return <p>Loading..</p>
+    }
     return (
         <>
+            <div>
+                {postData && postData.map(item => (
+                    <p key={item.id}>{item.brand}</p>
+                ))}
 
+            </div>
         </>
     )
 }
