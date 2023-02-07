1e7c6e85e5c21acb2b010c9bbb4d0927e582469a
arnab9593 <arnabpal679@gmail.com>
2023-02-07 20:17:03 +0530

    release 8

--- cake-shop/src/Components/Home.jsx
+++ cake-shop/src/Components/Home.jsx
@@ -1,4 +1,16 @@
-import { Button, ButtonGroup, Card, CardBody, CardFooter, Divider, Flex, Heading, Image, Stack, Text, VStack } from '@chakra-ui/react';
+import {
+    Button,
+    ButtonGroup,
+    Card,
+    CardBody,
+    CardFooter,
+    Divider,
+    Heading,
+    Image,
+    SimpleGrid,
+    Stack,
+    Text,
+} from '@chakra-ui/react';
 import React, { useEffect, useState } from 'react'
 import noimage from "../Images/noimage.jpg"
 
@@ -31,42 +43,40 @@ function Home() {
         return <p>Loading..</p>
     }
     return (
-        <Card maxW='sm'>
-            {postData && postData.map(item => (
-                <>
-                    <CardBody border={"1px solid black"} >
-                        <Image
-                            src={item.img}
-                            alt="image not found"
-                            borderRadius='lg'
-                        />
-                        <Stack mt='6' spacing='3'>
-                            <Heading size='md'>{item.name}</Heading>
-                            {/* <Text>
-                                This sofa is perfect for modern tropical spaces, baroque inspired
-                                spaces, earthy toned spaces and for people who love a chic design with a
-                                sprinkle of vintage design.
-                            </Text> */}
-                            <Text color='blue.600' fontSize='2xl'>
-                                $450
-                            </Text>
-                        </Stack>
-                        <Divider />
-                        <CardFooter>
-                            <ButtonGroup spacing='2'>
-                                <Button variant='solid' colorScheme='blue'>
-                                    Buy now
-                                </Button>
-                                <Button variant='ghost' colorScheme='blue'>
-                                    Add to cart
-                                </Button>
-                            </ButtonGroup>
-                        </CardFooter>
-                    </CardBody>
-                </>
-            ))}
-
-        </Card>
+        <>
+            <SimpleGrid columns={4} spacing={10} padding={10} >
+                {postData && postData.map(item => (
+                    <Card maxW='sm'>
+                        <CardBody border={"1px solid black"} borderRadius={5}>
+                            <Image
+                                h={200}
+                                w={"100%"}
+                                src={item.img}
+                                alt={item.name + "'s image not found"}
+                                borderRadius='lg'
+                            />
+                            <Stack mt='6' spacing='3'>
+                                <Heading size='md'>{item.name}</Heading>
+                                <Text color='blue.600' fontSize='2xl'>
+                                    $450
+                                </Text>
+                            </Stack>
+                            <Divider />
+                            <CardFooter>
+                                <ButtonGroup spacing='2'>
+                                    <Button variant='solid' colorScheme='blue'>
+                                        Buy now
+                                    </Button>
+                                    <Button variant='ghost' colorScheme='blue'>
+                                        Add to cart
+                                    </Button>
+                                </ButtonGroup>
+                            </CardFooter>
+                        </CardBody>
+                    </Card>
+                ))}
+            </SimpleGrid>
+        </>
     )
 }
 
